e2a7fd42eba736f15b618b8cd520f343ec22b2c8
Gunel Huseynova <gorellik246@gmail.com>
2020-10-03 21:41:15 +0300

    feat: write min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (arguments.length !== 0 && !Array.isArray(arguments[0])) {
+        return Math.min(...Array.from(arguments));
+    } else if (Array.isArray(array) && array.length !== 0) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (arguments.length !== 0 && !Array.isArray(arguments[0])) {
+        return Math.max(...Array.from(arguments));
+    } else if (Array.isArray(array) && array.length !== 0) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (arguments.length !== 0 && !Array.isArray(arguments[0])) {
+        return (
+            Array.from(arguments).reduce((a, b) => a + b, 0) /
+            Array.from(arguments).length
+        );
+    } else if (Array.isArray(array) && array.length !== 0) {
+        return array.reduce((a, b) => a + b, 0) / array.length;
+    } else {
+        return 0;
+    }
+};
